07f3c60d5cc43bfc8b99cc96aa6234d2fd01fff8
Chamanthi Aluri <143573994+alurichamanthi@users.noreply.github.com>
2023-10-25 21:14:36 -0500

    computation

--- routes/computation.js
+++ routes/computation.js
@@ -1,38 +1,21 @@
-const express = require('express');
-const app = express();
-
-
-router.get('/', (req, res) => {
-  // Get the last digit of your ID (replace this with your actual ID retrieval method)
-  const id = parseInt(req.query.id);
-  const lastDigit = id % 10;
-
-  // Check if a custom 'x' value is provided in the query string
-  const customX = parseFloat(req.query.x);
-
-  let x;
-  if (!isNaN(customX)) {
-    x = customX;
-  } else {
-    // Generate a random value between 1 and 100
-    x = Math.random() * 99 + 1;
-  }
-
-  let fn, result;
-
-  // Determine the function to apply based on the last digit
-  if (lastDigit === 0) {
-    fn = "log10";
-    result = math.log10(x);
-  } else {
-    fn = "unknown";
-    result = "unsupported function";
-  }
-
-  // Construct the response string
-  const response = `${fn} applied to ${x} is ${result}`;
-
-  res.send(response);
-});
-
+var express = require('express');
+var router = express.Router();
+ 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //router.get('/', (req, res) => {
+    // Check if there is a query parameter 'x' in the request
+    const x = req.query.x ? parseFloat(req.query.x) : Math.random() * 10;
+ 
+    // Calculate the sine of 'x'
+    const result = Math.log(x);
+ 
+    // Create the response string
+    const responseString = `Math.log() applied to ${x} is ${result}`;
+ 
+    res.send(responseString);
+  });
+ 
+//});
+ 
 module.exports = router;
